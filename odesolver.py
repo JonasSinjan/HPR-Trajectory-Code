acceleration bug fixed = still not entirely sure on the graph - accel should be gradient of velocity-time graph

odesolver.py
@@ -62,40 +62,40 @@ def accel(m, v, phase):
         u_e = 2175
         return - 9.81 - (0.5 * rho * v ** 2) * C_d * A / m + (mdot * u_e) / m
 
-    if phase == 1:  # coasting
+    elif phase == 1:  # coasting
         return - 9.81 - (0.5 * rho * v ** 2) * C_d * A / m
 
-    if phase == 2:  # upper stage
+    elif phase == 2:  # upper stage
         mdot = 0.1915  # kg/s - mass flow rate
         u_e = 1947.8
         return - 9.81 - (0.5 * rho * v ** 2) * C_d * A / m + (mdot * u_e) / m
 
 
-m_init = 4.2  # initial fully loaded mass in kg
-
+m_init = 4.4  # initial fully loaded mass in kg
+num = 100
 # 1st burn
 z0 = [0, 0, m_init]  # setting initial conditions [height, velocity, mass]
 t_end = 0.2472 / 0.2225
-t = np.linspace(0, t_end, 1000)
-sol = spi.solve_ivp(stage1, (0, t_end), z0, t_eval=t, max_step=0.001)
+t = np.linspace(0, t_end, num)
+sol = spi.solve_ivp(stage1, (0, t_end), z0, t_eval=t)
 
 # Coast for 1 second
 z1 = [sol.y[0, -1], sol.y[1, -1], sol.y[2, -1]]
 ct = t_end + 0.5
-t2 = np.linspace(t_end, ct, 1000)
-sol2 = spi.solve_ivp(coast, (t_end, ct), z1, t_eval=t2, max_step=0.001)
+t2 = np.linspace(t_end, ct, num)
+sol2 = spi.solve_ivp(coast, (t_end, ct), z1, t_eval=t2)
 
 # 2nd Burn
 z2 = [sol2.y[0, -1], sol2.y[1, -1], sol2.y[2, -1] - 1.995]  # taking into account bottom stage mass loss
 t2_end = 0.337 / 0.1915
-t3 = np.linspace(ct, ct + t2_end, 1000)
-sol3 = spi.solve_ivp(stage2, (ct, ct + t2_end), z2, t_eval=t3, max_step=0.001)
+t3 = np.linspace(ct, ct + t2_end, num)
+sol3 = spi.solve_ivp(stage2, (ct, ct + t2_end), z2, t_eval=t3)
 
 # Final Coast
 z3 = [sol3.y[0, -1], sol3.y[1, -1], sol3.y[2, -1]]
 cstart = ct + t2_end
-t4 = np.linspace(cstart, cstart + 17, 1000)
-sol4 = spi.solve_ivp(coast, (cstart, cstart + 17), z3, t_eval=t4, max_step=0.001)
+t4 = np.linspace(cstart, cstart + 17, num)
+sol4 = spi.solve_ivp(coast, (cstart, cstart + 17), z3, t_eval=t4)
 
 time = np.concatenate((sol.t, sol2.t, sol3.t, sol4.t))
 
@@ -107,36 +107,36 @@ print(f"The Max velocity is {max(velocity)} m/s")
 
 mass = np.concatenate((sol.y[2, :], sol2.y[2, :], sol3.y[2, :], sol4.y[2, :]))
 
-boost_1 = [0]*1000
-for i in range(1000):
+boost_1 = [0]*num
+for i in range(num):
     m = sol.y[2,i]
-    for j in range(1000):
+    for j in range(num):
         v = sol.y[1,i]
         boost_1[i] = accel(m,v,0)
         break
 
-coast_1 = [0]*1000
-for i in range(1000):
+coast_1 = [0]*num
+for i in range(num):
     m = sol2.y[2,i]
-    for j in range(1000):
+    for j in range(num):
         v = sol2.y[1,i]
-        boost_1[i] = accel(m,v,1)
+        coast_1[i] = accel(m,v,1)
         break
 
-boost_2 = [0]*1000
-for i in range(1000):
+boost_2 = [0]*num
+for i in range(num):
     m = sol3.y[2,i]
-    for j in range(1000):
+    for j in range(num):
         v = sol3.y[1,i]
-        boost_1[i] = accel(m,v,2)
+        boost_2[i] = accel(m,v,2)
         break
 
-coast_2 = [0]*1000
-for i in range(1000):
+coast_2 = [0]*num
+for i in range(num):
     m = sol4.y[2,i]
-    for j in range(1000):
+    for j in range(num):
         v = sol4.y[1,i]
-        boost_1[i] = accel(m,v,1)
+        coast_2[i] = accel(m,v,1)
         break
 
 